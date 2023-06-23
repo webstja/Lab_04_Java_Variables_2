294e1a8582dfefc662b7751af464552dbe1ea45c
webstja <136018686+webstja@users.noreply.github.com>
2023-06-23 12:39:34 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int numKids = 0;
+            System.out.println("The number of kids in my family is " + numKids + ".");
+        boolean isItRaining = false;
+            System.out.println("It is raining: " + isItRaining);
+        double gasPrice = 3.51;
+            System.out.println("The price of a gallon of gas is $" + gasPrice + ".");
+        int favNum = 18;
+            System.out.println("My favorite number is " + favNum + ".");
+        int shoeSize = 7;
+            System.out.println("My shoe size is " + shoeSize + ".");
+        String birthMonth = "February";
+            System.out.println("My birth month is " + birthMonth + ".");
+        String fullName = "Jaden Webster";
+            System.out.println("My full name is " + fullName + ".");
+    }
+}
